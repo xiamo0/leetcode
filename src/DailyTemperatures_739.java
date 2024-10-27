18ea773d592427d549df134eaf755f387ac3bc64
xiamo0 <65656443+xiamo0@users.noreply.github.com>
2024-10-27 12:30:42 -0300

    stack

--- src/DailyTemperatures_739.java
+++ src/DailyTemperatures_739.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * https://leetcode.cn/problems/daily-temperatures/description/?envType=problem-list-v2&envId=2cktkvj
  */
@@ -5,20 +7,40 @@ public class DailyTemperatures_739 {
 
 
     public static void main(String[] args) {
-
+        int[] answer = new int[10];
+        System.out.println(answer[1]);
     }
-    public static int[] dailyTemperatures(int[] temperatures) {
-        int[] answer=new int[temperatures.length];
+
+    public static int[] dailyTemperatures2(int[] temperatures) {
+        int[] answer = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+
         for (int i = 0; i < temperatures.length; i++) {
-            answer[i]=tap(i,temperatures);
+
+            int curTemp = temperatures[i];
+            while (!stack.isEmpty() && temperatures[stack.peek()] <curTemp ){
+               answer[stack.peek()] = i - stack.peek();
+               stack.pop();
+            }
+            stack.push(i);
+
         }
         return answer;
     }
-    public static int tap(int index,int[] temperatures) {
-        int cur=temperatures[index];
-        for (int i = index +1; i <temperatures.length ; i++) {
-            if (temperatures[i]>cur){
-                return i-index;
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int[] answer = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            answer[i] = tap(i, temperatures);
+        }
+        return answer;
+    }
+
+    public static int tap(int index, int[] temperatures) {
+        int cur = temperatures[index];
+        for (int i = index + 1; i < temperatures.length; i++) {
+            if (temperatures[i] > cur) {
+                return i - index;
             }
         }
         return 0;
---
+++ src/MaximalSquare_221.java
@@ -0,0 +1,42 @@
+/**
+ * https://leetcode.cn/problems/maximal-square/description/?envType=problem-list-v2&envId=2cktkvj
+ */
+public class MaximalSquare_221 {
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        int rowLength = matrix.length;
+        int columLength = matrix[0].length;
+        for (int curRow = 0; curRow < rowLength; curRow++) {
+            for (int curCol = 0; curCol < columLength; curCol++) {
+
+            }
+        }
+        return maxSide;
+
+    }
+
+
+    public int maximalSquare1(char[][] matrix) {
+        // base condition
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return 0;
+
+        int height = matrix.length;
+        int width = matrix[0].length;
+        int maxSide = 0;
+
+        // 相当于已经预处理新增第一行、第一列均为0
+        int[][] dp = new int[height + 1][width + 1];
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                if (matrix[row][col] == '1') {
+                    dp[row + 1][col + 1] = Math.min(Math.min(dp[row + 1][col], dp[row][col + 1]), dp[row][col]) + 1;
+                    maxSide = Math.max(maxSide, dp[row + 1][col + 1]);
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+
+
+}
