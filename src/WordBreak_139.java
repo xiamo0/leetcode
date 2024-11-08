7a841c58a57080eb06590de8cab9c6809c4e3524
xiamo0 <65656443+xiamo0@users.noreply.github.com>
2024-11-08 12:38:17 -0300

    leetcode
    dp usage

--- src/WordBreak_139.java
+++ src/WordBreak_139.java
@@ -9,21 +9,28 @@ import java.util.Optional;
 public class WordBreak_139 {
 
     public static void main(String[] args) {
-        String s="leetcode";
-        String[] wordDict={"leet", "code"};
+        String s = "leetcode";
+        String[] wordDict = {"leet", "code"};
         System.out.println(wordBreak(s, Arrays.stream(wordDict).toList()));
     }
+
     public static boolean wordBreak(String s, List<String> wordDict) {
 
         if (s == null || wordDict == null || wordDict.size() == 0) {
             return false;
         }
-        char[] chars=s.toCharArray();
-        for (int i = 1; i <chars.length ; i++) {
-            String substring = s.substring(0, i);
-            wordDict.stream()
-
-
+        char[] chars = s.toCharArray();
+        int[] dp = new int[chars.length + 1];
+        dp[0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                String substring = s.substring(j, i);
+                if (dp[j] == 1 && wordDict.contains(substring)) {
+                    dp[i] = 1;
+                    break;
+                }
+            }
         }
+        return dp[chars.length]==1;
     }
 }
