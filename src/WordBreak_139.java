4f034a49bf6daeff05619c4d3a42ffff59735fb2
xiamo0 <65656443+xiamo0@users.noreply.github.com>
2024-11-08 10:45:55 -0300

    leetcode

---
+++ src/WordBreak_139.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * https://leetcode.cn/problems/word-break/description/?envType=problem-list-v2&envId=2cktkvj
+ */
+public class WordBreak_139 {
+
+    public static void main(String[] args) {
+        String s="leetcode";
+        String[] wordDict={"leet", "code"};
+        System.out.println(wordBreak(s, Arrays.stream(wordDict).toList()));
+    }
+    public static boolean wordBreak(String s, List<String> wordDict) {
+
+        if (s == null || wordDict == null || wordDict.size() == 0) {
+            return false;
+        }
+        char[] chars=s.toCharArray();
+        for (int i = 1; i <chars.length ; i++) {
+            String substring = s.substring(0, i);
+            wordDict.stream()
+
+
+        }
+    }
+}
