72ab6a2fa17bf987d94412a69fe394f6bf7679d0
xiamo0 <65656443+xiamo0@users.noreply.github.com>
2025-01-19 12:03:07 -0300

    leetcode

--- src/RemoveNthFromEnd.java
+++ src/RemoveNthFromEnd.java
@@ -1,62 +1,76 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class RemoveNthFromEnd {
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        int nodeCount = 0;
-        ListNode copyNode = head;
-        while (copyNode != null) {
-            nodeCount++;
-            copyNode = copyNode.next;
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        listNode1.next = listNode2;
+        removeNthFromEnd(listNode1, 1);
+
+    }
+
+    public static ListNode removeNthFromEnd2(ListNode head, int n) {
+        int length = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
         }
-        int removeNodeIndex = nodeCount - n + 1;
-        copyNode = head;
-        ListNode result = null;
-        ListNode temp = null;
-        int curIndex = 1;
-        while (curIndex <= nodeCount) {
-            int val = copyNode.val;
-            copyNode = copyNode.next;
-
-
-            if (result == null) {
-                result = new ListNode(val);
-                result.next = new ListNode();
-                temp = result.next;
-            } else {
-                temp.val = val;
-                temp.next = new ListNode();
-                temp = temp.next;
-            }
-            if (curIndex == removeNodeIndex) {
-
-            }else if
-
-
-            if (result == null) {
-                result = new ListNode(val);
-                result.next = new ListNode();
-                if ((curIndex + 1) == romoveNodeIndex) {
-                    result.next = null;
-                }
-                temp = result.next;
-            } else {
-                temp.val = val;
-                temp.next = new ListNode();
-                if (curIndex == nodeCount) {
-                    temp.next = null;
-                }
-                temp = temp.next;
-            }
-
-            curIndex++;
+        ListNode newHead = new ListNode(0, head);
+        temp = newHead;
+        for (int i = 0; i < length-n+1; i++) {
+            temp=temp.next;
         }
-        return result;
+        temp.next=temp.next.next;
+        return newHead.next;
 
 
     }
 
-    public class ListNode {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        int nodeCount = 0;
+        ListNode copyNode = head;
+        List<Integer> content = new ArrayList<>();
+        while (copyNode != null) {
+            nodeCount++;
+            content.add(copyNode.val);
+            copyNode = copyNode.next;
+        }
+        int removeNodeIndex = nodeCount - n;
+        content.remove(removeNodeIndex);
+        ListNode result = null;
+        ListNode tempNextNode = null;
+        for (int i = 0; i < content.size(); i++) {
+            Integer curVal = content.get(i);
+            if (curVal == null) {
+                continue;
+            }
+            if (result == null) {
+                result = new ListNode();
+                result.val = curVal;
+                if (i + 1 == content.size()) {
+                    result.next = null;
+                } else {
+                    result.next = new ListNode();
+                }
+                tempNextNode = result.next;
+            } else {
+                tempNextNode.val = curVal;
+                if (i + 1 == content.size()) {
+                    tempNextNode.next = null;
+                } else {
+                    tempNextNode.next = new ListNode();
+                }
+                tempNextNode = tempNextNode.next;
+            }
+        }
+        return result;
+    }
+
+    public static class ListNode {
         int val;
         ListNode next;
 
