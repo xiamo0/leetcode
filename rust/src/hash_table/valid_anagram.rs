db98cf4fe7459d8871eca2e808092b356e91d81e
xiamo0 <65656443+xiamo0@users.noreply.github.com>
2025-06-28 00:28:01 +0800

    hash table 4

--- rust/src/hash_table/valid_anagram.rs
+++ rust/src/hash_table/valid_anagram.rs
@@ -1,7 +1,16 @@
 use super::Solution;
+use std::collections::HashMap;
 impl Solution {
     pub fn is_anagram(s: String, t: String) -> bool {
-
+        if s.len() != t.len() {
+            return false;
+        }
+        let mut map = HashMap::new();
+        for (sc, tc) in s.chars().zip(t.chars()) {
+            *map.entry(sc).or_insert(0) += 1;
+            *map.entry(tc).or_insert(0) -= 1;
+        }
+        map.values().all(|&c| c == 0)
     }
 }
 #[cfg(test)]
@@ -12,6 +21,5 @@ mod tests {
     fn test_is_anagram() {
         let anagram = Solution::is_anagram("anagram".to_string(), "nagaram".to_string());
         assert_eq!(anagram, true);
-
     }
 }
