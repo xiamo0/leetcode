3b4f4efff35e37c8e957569ed51a8f308812cbec
xiamo0 <65656443+xiamo0@users.noreply.github.com>
2025-06-30 23:41:06 +0800

    hash table 5

--- rust/src/hash_table/word_pattern.rs
+++ rust/src/hash_table/word_pattern.rs
@@ -1,7 +1,31 @@
 use super::Solution;
+use std::collections::HashMap;
 
 impl Solution {
-    pub fn word_pattern(pattern: String, s: String) -> bool {}
+    pub fn word_pattern(pattern: String, s: String) -> bool {
+        let pattern_vec: Vec<char> = pattern.chars().collect();
+        let s_vec: Vec<String> = s.split(" ").map(|s| s.to_string()).collect();
+        if pattern_vec.len() != s_vec.len() {
+            return false;
+        }
+        let mut map = HashMap::new();
+        for (&item1, item2) in pattern_vec.iter().zip(s_vec.iter()) {
+            let exists = map.values().any(|v| *v == item2);
+            let option = map.get(&item1);
+            if option.is_none() && exists {
+                return false;
+            }
+            if option.is_none() {
+                map.insert(item1, item2);
+                continue;
+            }
+            let x = option.unwrap();
+            if *x != item2 {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 #[cfg(test)]
 mod tests {
--- src/hashtable/WordPattern.java
+++ src/hashtable/WordPattern.java
@@ -1,4 +1,7 @@
 package hashtable;
+
+import java.util.HashMap;
+
 /*
 https://leetcode.cn/problems/word-pattern/description/?envType=problem-list-v2&envId=hash-table
 单词规律
@@ -20,6 +23,29 @@ https://leetcode.cn/problems/word-pattern/description/?envType=problem-list-v2&e
  */
 public class WordPattern {
     public boolean wordPattern(String pattern, String s) {
+        String[] split = pattern.split("");
+        String[] split1 = s.split(" ");
+        if (split.length != split1.length) {
+            return false; // 长度不同，直接返回false
+        }
+        HashMap<String, String> map = new HashMap<>();
+        for (int i1 = 0; i1 < split.length; i1++) {
+            String s1 = split[i1];
+            String s2 = split1[i1];
+            String s3 = map.get(s1);
+            if (s3==null && map.containsValue(s2)){
+                return false; // 如果s2已经被其他s1映射，返回false
+            }
+            if (s3==null){
+                map.put(s1, s2);
+                continue;
+            }
+            if (!s3.equals(s2)) {
+                return false; // 如果映射不一致，返回false
+            }
+
+        }
+        return true;
 
     }
 }
