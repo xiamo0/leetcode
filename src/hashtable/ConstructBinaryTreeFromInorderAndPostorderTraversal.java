72ebf93059409ba76157577cf80214be13492ec9
xiamo0 <65656443+xiamo0@users.noreply.github.com>
2025-07-03 22:38:20 +0800

    hash table 7

--- src/hashtable/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ src/hashtable/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -11,6 +11,63 @@ https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-tr
  */
 public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder.length == 0 || postorder.length == 0) {
+            return null;
+        }
+        return buildTree(inorder, postorder, 0, inorder.length - 1, 0, postorder.length - 1);
+    }
 
+    private TreeNode buildTree(int[] inorder, int[] postorder, int inStart, int inEnd, int postStart, int postEnd) {
+        if (inStart > inEnd || postStart > postEnd) {
+            return null;
+        }
+        // 后序遍历的最后一个元素是根节点
+        int rootVal = postorder[postEnd];
+        TreeNode root = new TreeNode(rootVal);
+
+        // 在中序遍历中找到根节点的位置
+        int rootIndex = -1;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == rootVal) {
+                rootIndex = i;
+                break;
+            }
+        }
+
+        // 计算左子树和右子树的大小
+        int leftSize = rootIndex - inStart;
+
+        // 构建左子树和右子树
+        root.left = buildTree(inorder, postorder, inStart, rootIndex - 1, postStart, postStart + leftSize - 1);
+        root.right = buildTree(inorder, postorder, rootIndex + 1, inEnd, postStart + leftSize, postEnd - 1);
+
+        return root;
+    }
+
+    private int[] inorder(TreeNode node) {
+
+        if (node == null) {
+            return new int[0];
+        }
+        int[] left = inorder(node.left);
+        int[] right = inorder(node.right);
+        int[] result = new int[left.length + right.length + 1];
+        System.arraycopy(left, 0, result, 0, left.length);
+        result[left.length] = node.val;
+        System.arraycopy(right, 0, result, left.length + 1, right.length);
+        return result;
+    }
+
+    private int[] postorder(TreeNode node) {
+        if (node == null) {
+            return new int[0];
+        }
+        int[] left = postorder(node.left);
+        int[] right = postorder(node.right);
+        int[] result = new int[left.length + right.length + 1];
+        System.arraycopy(left, 0, result, 0, left.length);
+        System.arraycopy(right, 0, result, left.length, right.length);
+        result[left.length + right.length] = node.val;
+        return result;
     }
 }
